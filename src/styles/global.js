7eb3a6d9d1686b2f41e5da49425202ddb37a9897
João Carlos Nepomuceno Fernandes <joao.carlos.nepomuceno06@aluno.ifce.edu.br>
2022-05-24 14:37:59 -0300

    New Project
    New Project

---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    background-color: #a8bcbd;
+
+}
+
+`;
+
+export default GlobalStyle;
